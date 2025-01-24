a101bdac02aa67baa7cc7ec68dac9cc88631c553
Leandro Rodrigues <leandrorbernardes@outlook.com>
2025-01-24 17:40:54 +0000

    Novo Commit # Please enter the commit message for your changes. Lines starting

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,7 +7,8 @@
 //Teste larissa
 
 int main() {
-        printf ("Desafio SuperTrunfo\n")
+        printf ("Desafio SuperTrunfo\n");
+        printf("Novo commit\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
